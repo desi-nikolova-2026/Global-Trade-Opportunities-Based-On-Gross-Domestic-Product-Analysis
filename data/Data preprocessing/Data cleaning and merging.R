f50c683af0315856ed6cf1435db51c615841a50f
desi-nikolova-2026 <desislava.miteva.nikolova@proton.me>
2026-07-24 12:12:42 +0300

    Working on pre-processed data file formatting.

--- data/Data preprocessing/Data cleaning and merging.R
+++ data/Data preprocessing/Data cleaning and merging.R
@@ -1,27 +1,27 @@
 # Load the data:
 # see the reference here - https://readxl.tidyverse.org/
-
 install.packages("tidyverse")
 install.packages("readxl")
 install.packages("pak")
 pak::pak("tidyverse/readxl")
-library(readxl)
+
 
 # Read the file and skip the header inserted by The World Bank (first 3 rows):
-
+library(readxl)
 xls_data_Trade_GDP <- read_excel("API_NE.TRD.GNFS.ZS_DS2_en_excel_v2_33090.xls", 
                                  sheet='Data',
                                  skip = 3)
 xls_data_Trade_GDP
 
-# Remove ALL rows until Country Name:
+# TODO - add column names when exporting the excel file:
 
 #TODO
+### perhaps this can help : https://www.biostars.org/p/469010/
 
 # Save the Excel file that should be used in MS PowerBI:
 
 library(writexl)
-write_xlsx(xls_data_Trade_GDP, 'Trade_GDP_Preprocessed.xls')
-file.exists('Trade_GDP_Preprocessed.xls')
+write_xlsx(xls_data_Trade_GDP, 'Trade_GDP_Preprocessed.xlsx')
+file.exists('Trade_GDP_Preprocessed.xlsx')
 
-# TODO - add column names when exporting the excel file*
+
