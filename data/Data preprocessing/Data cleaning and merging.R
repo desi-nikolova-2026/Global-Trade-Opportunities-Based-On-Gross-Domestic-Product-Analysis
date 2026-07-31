ae52b32d726229bd95274b0e1374261c5d8a34a9
desi-nikolova-2026 <desislava.miteva.nikolova@proton.me>
2026-07-31 10:19:25 +0300

    Intial analysis

--- data/Data preprocessing/Data cleaning and merging.R
+++ data/Data preprocessing/Data cleaning and merging.R
@@ -1,4 +1,4 @@
-# Load the data:
+# 1. Load the data:
 # see the reference here - https://readxl.tidyverse.org/
 install.packages("tidyverse")
 install.packages("readxl")
@@ -6,22 +6,25 @@ install.packages("pak")
 pak::pak("tidyverse/readxl")
 
 
-# Read the file and skip the header inserted by The World Bank (first 3 rows):
+# 2. Read the file and skip the header inserted by The World Bank (first 3 rows):
 library(readxl)
 xls_data_Trade_GDP <- read_excel("API_NE.TRD.GNFS.ZS_DS2_en_excel_v2_33090.xls", 
                                  sheet='Data',
                                  skip = 3)
 xls_data_Trade_GDP
 
-# TODO - add column names when exporting the excel file:
+noquote(colnames(xls_data_Trade_GDP))# <- c("New_Name1", "New_Name2", "New_Name3")
+column_names <- noquote(colnames(xls_data_Trade_GDP))
+colnames(xls_data_Trade_GDP) <- column_names
+# 3. TODO!!! - Add column names when exporting the excel file:
 
-#TODO
+### the column names have a warning when opened in Excel 
 ### perhaps this can help : https://www.biostars.org/p/469010/
 
-# Save the Excel file that should be used in MS PowerBI:
 
+# 4. Save the Excel file that should be used in MS PowerBI:
 library(writexl)
-write_xlsx(xls_data_Trade_GDP, 'Trade_GDP_Preprocessed.xlsx')
+write_xlsx(xls_data_Trade_GDP, 'Trade_GDP_Preprocessed.xlsx', col_names = TRUE)
 file.exists('Trade_GDP_Preprocessed.xlsx')
 
 
